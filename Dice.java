ceaa8d0b96ffc190dd7acf291e0fef00580a7537
amalozzi300 <95106625+amalozzi300@users.noreply.github.com>
2021-12-01 16:44:29 -0500

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-import java.awt.*;
 import java.util.*;
 
 public class Dice
@@ -7,15 +5,12 @@ public class Dice
   public class Die
   {
     protected int value;
-    protected ImageIcon image;
-    protected JLabel icon;
     
     private Random generator = new Random ();
     
     public Die ()
     {
       setNewValue ();
-      setIcon (value);
     }
     
     public void setNewValue ()
@@ -23,38 +18,35 @@ public class Dice
       value = generator.nextInt (6);
       value++;
     }
-    
-    public void setIcon (int val)
-    {
-      switch (val)
-      {
-        case 1:
-          image = new ImageIcon (getClass ().getResource ("die1.png"));
-          icon = new JLabel (image);
-          break;
-        case 2:
-          image = new ImageIcon (getClass ().getResource ("die2.png"));
-          icon = new JLabel (image);
-          break;
-        case 3:
-          image = new ImageIcon (getClass ().getResource ("die3.png"));
-          icon = new JLabel (image);
-          break;
-        case 4:
-          image = new ImageIcon (getClass ().getResource ("die4.png"));
-          icon = new JLabel (image);
-          break;
-        case 5:
-          image = new ImageIcon (getClass ().getResource ("die5.png"));
-          icon = new JLabel (image);
-          break;
-        case 6:
-          image = new ImageIcon (getClass ().getResource ("die6.png"));
-          icon = new JLabel (image);
-          break;
-      }
-    }
   }
   
+  private Die die;
+  public Die[] dice;
+  private boolean[] hold;
   
+  public Dice ()
+  {
+    dice = new Die[5];
+    hold = new boolean[5];
+    
+    for (int i = 0; i < 5; i++)
+    {
+      hold[i] = false;
+    }
+    
+    Roll ();
+  }
+  
+  public void Roll ()
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      if (!hold[i])
+      {
+        die = dice[i];
+        die.setNewValue ();
+        dice[i] = die;
+      }
+    } 
+  }
 }
