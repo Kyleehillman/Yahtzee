253b746f5d903e56696deea2f659b4f53afa3761
Kyleehillman <77276016+Kyleehillman@users.noreply.github.com>
2021-12-01 15:42:57 -0500

    Update and rename ScoreCard.java to Scorecard.java

--- Scorecard.java
+++ Scorecard.java
@@ -1,6 +1,6 @@
 import java.awt.event*;
-import java.awt.Color;
-import java.awt.GridLayout;
+//import java.awt.Color;
+//import java.awt.GridLayout;
 import javax.swing*;
 import java.awt.Dimension;
 
