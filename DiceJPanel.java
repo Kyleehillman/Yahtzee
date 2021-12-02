6bf764e5d38e7f18d0c3699ac1aa24b2969cdabf
amalozzi300 <95106625+amalozzi300@users.noreply.github.com>
2021-12-01 20:48:25 -0500

    Update DiceJPanel.java

--- DiceJPanel.java
+++ DiceJPanel.java
@@ -5,10 +5,46 @@ public class DiceJPanel extends JPanel implements ActionListener
 {
   public void paintComponent (Graphics g)
   {
+    Dice dice = new Dice ();
+    
+    ImageIcon = image;
+    
+    JButton diceButton;
+    JButton rollButton;
+  
     setPrefferedSize (new Dimension (1500, 500));
     
     GridLayout diceGrid = new GridLayout (1, 5, 20, 0);
     
-    
+    for (int i = 0; i < 5; i++)
+    {
+      if (dice.getValue (i) == 1)
+      {
+        image = new ImageIcon (getClass ().getResource ("die1.png"));
+      }
+      else if (dice.getValue (i) == 2)
+      {
+        image = new ImageIcon (getClass ().getResource ("die2.png"));
+      }
+      else if (dice.getValue (i) == 3)
+      {
+        image = new ImageIcon (getClass ().getResource ("die3.png"));
+      }
+      else if (dice.getValue (i) == 4)
+      {
+        image = new ImageIcon (getClass ().getResource ("die4.png"));
+      }
+      else if (dice.getValue (i) == 5)
+      {
+        image = new ImageIcon (getClass ().getResource ("die5.png"));
+      }
+      else if (dice.getValue (i) == 6)
+      {
+        image = new ImageIcon (getClass ().getResource ("die6.png"));
+      }
+      
+      diceButton = new JButton (image);
+      add (diceButton);
+    }
   }
 }
