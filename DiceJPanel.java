c4ca5283f14787b5ec14486cc1da8323d82aacad
Dori Palmer <95323142+doripalmer@users.noreply.github.com>
2021-12-02 16:56:26 -0500

    Update DiceJPanel.java

--- DiceJPanel.java
+++ DiceJPanel.java
@@ -5,18 +5,19 @@ public class DiceJPanel extends JPanel implements ActionListener
 {
   public void paintComponent (Graphics g)
   {
-    Dice dice = new Dice ();
+    Dice dice = new Dice ();                //gets a new dice
     
-    ImageIcon image;
+    ImageIcon image;                        //variable for picture of dice
     
-    JButton die1, die2, die3, die4, die5;
-    JButton rollButton;
+    JButton die1, die2, die3, die4, die5;   //makes each die into a button
+    JButton rollButton;                     //creates a roll button
   
-    setPrefferedSize (new Dimension (1500, 500));
+    setPrefferedSize (new Dimension (1500, 500));   //edits layout
     
-    setLayout (new GridLayout (1, 5, 20, 0));
+    setLayout (new GridLayout (1, 5, 20, 0));       //edits layout
     
     for (int i = 0; i < 5; i++)
+    //adds the picture for each value rolled
     {
       if (dice.getValue (i) == 1)
       {
@@ -44,30 +45,35 @@ public class DiceJPanel extends JPanel implements ActionListener
       }
       
       if (i == 0)
+      //if die one is chosen to keep
       {
-        die1 = new JButton (image);
+        die1 = new JButton (image); 
         add (die1);
         die1.setVisible (true);
       }
       else if (i == 1)
+      //if die two is chosen to keep
       {
         die2 = new JButton (image);
         add (die2);
         die2.setVisible (true);
       }
       else if (i == 2)
+      //if die three is chosen to keep
       {
         die3 = new JButton (image);
         add (die3);
         die3.setVisible (true);
       }
       else if (i == 3)
+      //if die four is chosen to keep
       {
         die4 = new JButton (image);
         add (die4);
         die4.setVisible (true);
       }
       else
+      //if die five is chosen to keep
       {
         die5 = new JButton (image);
         add (die5);
